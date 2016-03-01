81d66205290eb394ca4a6ad3804e4b196e94cce4
Jan Lelis <mail@janlelis.de>
2016-03-01 01:03:38 +0100

    Separate from front-end version

--- yyid.js
+++ yyid.js
@@ -1,32 +1,14 @@
 'use strict';
 
-if( typeof process !== 'undefined' &&
-    {}.toString.call(process) === '[object process]' ) {
+var crypto = require('crypto');
 
-  // Node
-  (function(){
-    var crypto = require('crypto');
-    module.exports = function yyid() {
-      var randomBytes = crypto.randomBytes(16);
-      return randomBytes.slice( 0, 4).toString('hex') + '-' +
-             randomBytes.slice( 4, 6).toString('hex') + '-' +
-             randomBytes.slice( 6, 8).toString('hex') + '-' +
-             randomBytes.slice( 8,10).toString('hex') + '-' +
-             randomBytes.slice(10,16).toString('hex');
-    };
-  })();
-} else {
+var yyid = function yyid() {
+  var randomBytes = crypto.randomBytes(16);
+  return randomBytes.slice( 0, 4).toString('hex') + '-' +
+         randomBytes.slice( 4, 6).toString('hex') + '-' +
+         randomBytes.slice( 6, 8).toString('hex') + '-' +
+         randomBytes.slice( 8,10).toString('hex') + '-' +
+         randomBytes.slice(10,16).toString('hex');
+};
 
-  // Browser
-  (function(global) {
-    global.yyid = function yyid() {
-      var i = 0;
-      var randomTwoBytes = new Uint16Array(8);
-      crypto.getRandomValues(randomTwoBytes);
-
-      return 'xx-x-x-x-xxx'.replace(/x/g, function() {
-        return ('000' + randomTwoBytes[i++].toString(16)).substr(-4);
-      });
-    }
-  }(this));
-}
+module.exports = yyid;
