5228144303bd2a94d341c853925abfd01d1f4df6
Jan Lelis <mail@janlelis.de>
2016-03-01 01:55:11 +0100

    Fix specs

--- spec/yyid_spec.js
+++ spec/yyid_spec.js
@@ -1,12 +1,6 @@
 'use strict';
 
-if (typeof(yyid) === 'undefined') {
-  var isNode = true;
-  var yyid = require('../yyid');
-} else {
-  var isNode = false;
-}
-
+var yyid = require('../index');
 
 describe('yyid()', function() {
   it('returns a string', function() {
@@ -17,18 +11,10 @@ describe('yyid()', function() {
     expect( yyid() ).toMatch(/^\w{8}-\w{4}-\w{4}-\w{4}-\w{12}$/);
   });
 
-  if(isNode){
-    it('uses the the node crypto api', function() {
-      var crypto = require('crypto');
-      spyOn(crypto, 'randomBytes').and.callThrough();
-      yyid();
-      expect(crypto.randomBytes).toHaveBeenCalled();
-    });
-  } else {
-    it('uses the the web crypto api', function() {
-      spyOn(crypto, 'getRandomValues').and.callThrough();
-      yyid();
-      expect(crypto.getRandomValues).toHaveBeenCalled();
-    });
-  }
+  it('uses the the node crypto api', function() {
+    var crypto = require('crypto');
+    spyOn(crypto, 'randomBytes').and.callThrough();
+    yyid();
+    expect(crypto.randomBytes).toHaveBeenCalled();
+  });
 });
